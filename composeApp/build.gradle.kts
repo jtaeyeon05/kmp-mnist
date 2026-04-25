d4c5a0f6a78a6bb4fe30b2d14548c65181a64bc6
jtaeyeon05 <jtaeyeon05@gmail.com>
2026-04-26 04:56:28 +0900

    feat(ui): Improve icons
    - Update icons.

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -13,8 +13,8 @@ plugins {
     alias(libs.plugins.composeHotReload)
 }
 
-val releaseName = "1.3.2"
-val releaseCode = 34
+val releaseName = "1.3.3"
+val releaseCode = 35
 val buildNumber = LocalDateTime.now(ZoneId.of("Asia/Seoul")).format(DateTimeFormatter.ofPattern("yyyyMMddHHmmss"))!!
 val buildInfoPackage = "io.github.jtaeyeon05.kmp_mnist.buildinfo"
 val buildInfoDir = layout.buildDirectory.dir("generated/sources/buildInfo/kotlin")
